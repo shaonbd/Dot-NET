7f95e82daf09b9dd4d2c6d51b57b5e9b05d49de3
shaonbd <silent.shaon@gmail.com>
2017-01-02 23:30:01 +0600

    Add files via upload

---
+++ Collections/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList al = new ArrayList();
+            Console.WriteLine(al.Capacity);
+            al.Add("kamal");
+            al.Add("Tahmid");
+            al.Add("kamal");
+            al.Add("Tahmid");
+            al.Add("kamal");
+            al.Add("Tahmid");
+            Console.WriteLine(al.Capacity);
+            Console.WriteLine(al.Count);
+            Console.ReadKey();
+
+           
+
+        }
+    }
+}
