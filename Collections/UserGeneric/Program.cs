5e40314ae0108bc6d0b3c1a2705060dd3e0661ba
shaonbd <silent.shaon@gmail.com>
2017-01-02 23:39:07 +0600

    Add files via upload

---
+++ Collections/UserGeneric/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserGeneric
+{
+    class Test<T>
+    {
+        T _Num;
+
+        public Test(T t)
+        {
+           
+            this._Num = t;
+        }
+
+        public void Write()
+        {
+            Console.WriteLine(this._Num);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+           
+            Test<int> test1 = new Test<int>(5);
+            test1.Write();
+            Test<string> test2 = new Test<string>("cat");
+            test2.Write();
+           Console.ReadKey();
+        }
+    }
+
+}
---
+++ Collections/Yieldprgs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yieldprgs
+{
+    class Program
+    {
+        static List<int> mylist = new List<int>();
+        static void Fillvalues()
+        {
+            mylist.Add(1);
+            mylist.Add(2);
+            mylist.Add(3);
+            mylist.Add(4);
+            mylist.Add(5);
+        }
+
+        static void Main(string[] args)
+        {
+            Fillvalues();
+
+            foreach (int item in Filter())
+            {
+                Console.WriteLine(item); 
+            }
+
+            Console.ReadKey();
+        }
+
+
+        static IEnumerable<int> Filter()
+        {
+           // List<int> temp = new List<int>();
+            foreach (int i  in mylist)
+            {
+                if (i > 3)
+                {
+                   // temp.Add(i);
+                    yield return i;
+                }
+           
+            }
+           // return temp;
+
+
+        }
+
+    }
+}
