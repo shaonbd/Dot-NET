dc55e538854c25378995773a85e4b80aba92e8aa
shaonbd <silent.shaon@gmail.com>
2017-01-02 23:30:32 +0600

    Add files via upload

---
+++ Collections/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList obj = new ArrayList();
+
+            obj.Add("kamal");
+            obj.Add(1);
+            obj.Add('w');
+
+
+            foreach (var item in obj)
+            {
+                Console.WriteLine(item);
+            }
+           
+
+
+
+          
+        }
+    }
+}
